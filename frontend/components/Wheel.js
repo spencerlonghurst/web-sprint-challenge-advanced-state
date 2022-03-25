0d06851042cf71e1c1948d33edbf696a82f72700
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-25 11:40:41 -0600

    Established state and can change state with buttons

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Wheel(props) {
+
+  const [wheel, setWheel] = useState({ "B": 0 })
+
+  const changeClockwise = () => {
+    setWheel({ ...wheel, 'B': wheel.B + 1 })
+  }
+
+  const changeCounterClockwise = () => {
+    setWheel({ ...wheel, 'B': wheel.B - 1 })
+  }
+
+
+console.log('wheel state:', wheel )
+
+
+
+
+
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
+        <div className={`cog ${wheel.B === 0 ? 'active' : ''}`} style={{ "--i": 0 }}>B</div>
         <div className="cog" style={{ "--i": 1 }}></div>
         <div className="cog" style={{ "--i": 2 }}></div>
         <div className="cog" style={{ "--i": 3 }}></div>
@@ -12,8 +30,8 @@ export default function Wheel(props) {
         <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={changeCounterClockwise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={changeClockwise}>Clockwise</button>
       </div>
     </div>
   )
