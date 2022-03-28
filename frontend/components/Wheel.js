72372f0add77497cbae51a173459ab1e08ce45e5
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-28 14:13:40 -0600

    MVP!!!!

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,41 +1,56 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import * as actionCreators from '../state/action-creators'
 
-export default function Wheel() {
 
-  const [wheel, setWheel] = useState({ "B": 0 })
 
-  const changeClockwise = () => {
-    if (wheel.B < 5) {
-      setWheel({ ...wheel, 'B': wheel.B + 1 })
-    } else {
-      setWheel({ ...wheel, 'B': wheel.B - 5 })
-    }
+
+export function Wheel(props) {
+
+  const {
+    wheel,
+    moveClockwise,
+    moveCounterClockwise,
+  } = props
+  
+  console.log(props);
+
+
+  const handleClickClockwise = evt => {
+    const { value } = evt.target;
+    moveClockwise(value)
   }
 
-  const changeCounterClockwise = () => {
-    if (wheel.B > 0) {
-      setWheel({ ...wheel, 'B': wheel.B - 1 })
-    } else {
-      setWheel({ ...wheel, 'B': wheel.B + 5 })
-
-    }
+  const handleClickCounterClockwise = evt => {
+    const { value } = evt.target;
+    moveCounterClockwise(value)
   }
 
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className={`cog ${wheel.B === 0 ? 'active' : ''}`} style={{ "--i": 0 }}>{`${wheel.B === 0 ? 'B' : ''}`}</div>
-        <div className={`cog ${wheel.B === 1 ? 'active' : ''}`} style={{ "--i": 1 }}>{`${wheel.B === 1 ? 'B' : ''}`}</div>
-        <div className={`cog ${wheel.B === 2 ? 'active' : ''}`} style={{ "--i": 2 }}>{`${wheel.B === 2 ? 'B' : ''}`}</div>
-        <div className={`cog ${wheel.B === 3 ? 'active' : ''}`} style={{ "--i": 3 }}>{`${wheel.B === 3 ? 'B' : ''}`}</div>
-        <div className={`cog ${wheel.B === 4 ? 'active' : ''}`} style={{ "--i": 4 }}>{`${wheel.B === 4 ? 'B' : ''}`}</div>
-        <div className={`cog ${wheel.B === 5 ? 'active' : ''}`} style={{ "--i": 5 }}>{`${wheel.B === 5 ? 'B' : ''}`}</div>
+        <div className={`cog ${wheel === 0 ? 'active' : ''}`} style={{ "--i": 0 }}>{`${wheel === 0 ? 'B' : ''}`}</div>
+        <div className={`cog ${wheel === 1 ? 'active' : ''}`} style={{ "--i": 1 }}>{`${wheel === 1 ? 'B' : ''}`}</div>
+        <div className={`cog ${wheel === 2 ? 'active' : ''}`} style={{ "--i": 2 }}>{`${wheel === 2 ? 'B' : ''}`}</div>
+        <div className={`cog ${wheel === 3 ? 'active' : ''}`} style={{ "--i": 3 }}>{`${wheel === 3 ? 'B' : ''}`}</div>
+        <div className={`cog ${wheel === 4 ? 'active' : ''}`} style={{ "--i": 4 }}>{`${wheel === 4 ? 'B' : ''}`}</div>
+        <div className={`cog ${wheel === 5 ? 'active' : ''}`} style={{ "--i": 5 }}>{`${wheel === 5 ? 'B' : ''}`}</div>
         {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" onClick={changeCounterClockwise}>Counter clockwise</button>
-        <button id="clockwiseBtn" onClick={changeClockwise}>Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleClickCounterClockwise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={handleClickClockwise}>Clockwise</button>
       </div>
     </div>
   )
 }
+
+// const mapStateToProps = state => {
+//   // console.log('state', state)
+//   return {
+//     wheel: state.wheel
+//   }
+// }
+
+
+export default connect(st => st, actionCreators)(Wheel)
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -39,14 +39,10 @@ export function fetchQuiz() {
     // - Dispatch an action to send the obtained quiz to its state
     axios.get('http://localhost:9000/api/quiz/next')
       .then(res => {
-        // console.log('GET res', res.data)
-        // dispatch({ type: SET_QUIZ_INTO_STATE, payload: })
         dispatch(setQuiz(res.data))
       })
       .catch(err => {
-        // console.log('GET err', err)
-        debugger
-        // dispatch({ type: null, payload: err.response.data.error})
+        dispatch({ type: null, payload: err.response.data.error})
 
       })
   }
@@ -59,15 +55,12 @@ export function postAnswer(quiz_id, answer_id) {
     // - Dispatch the fetching of the next quiz
     axios.post('http://localhost:9000/api/quiz/answer', { quiz_id, answer_id })
     .then(res => {
-      console.log('res', res)
       dispatch(selectAnswer(null));
       dispatch(setMessage(res.data.message));
-      console.log('message', res.data.message)
       dispatch(fetchQuiz())
     })
     .catch(err => {
-      debugger
-      // dispatch({ type: null, payload: err.response.data.error})
+      dispatch({ type: null, payload: err.response.data.error})
     })
   }
 }
@@ -78,13 +71,10 @@ export function postQuiz(input) {
     // - Dispatch the resetting of the form
     axios.post('http://localhost:9000/api/quiz/new', { question_text: input.newQuestion, true_answer_text: input.newTrueAnswer, false_answer_text: input.newFalseAnswer })
     .then(res => {
-      console.log('res.data.message',res.data.message)
       dispatch(setMessage(`Congrats: "${res.data.question}" is a great question!`))
       dispatch(resetForm())
-      debugger
     })
     .catch(err => {
-      console.log(err)
       debugger
     })
   }
